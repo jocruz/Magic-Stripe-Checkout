892273161733484c7a6ec51959584bb886d30509
John Cruz <johncruzag@gmail.com>
2023-06-23 12:16:39 -0400

    change connector

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,26 +7,16 @@ import "../styles/globals.css";
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Polygon;
 
-// const magicLinkConnector = new MagicConnector({
-//   options: {
-//     apiKey: process.env.NEXT_PUBLIC_MAGIC_LINK_API_KEY as string,
-//     rpcUrls: {
-//       [ChainId.Polygon]: "https://rpc-mainnet.matic.quiknode.pro",
-//     },
-//   },
-// });
-
-const magicLinkConnector =  magicLink({ apiKey: process.env.NEXT_PUBLIC_MAGIC_LINK_API_KEY as string,  type: 'connect' })
+const magicLinkConfig = magicLink({
+  apiKey: process.env.NEXT_PUBLIC_MAGIC_LINK_API_KEY as string, type: 'connect',
+});
 
 // Array of wallet connectors you want to use for your dApp.
-const connectors = [magicLinkConnector];
+// const connectors = [magicLinkConfig];
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider
-      activeChain={activeChainId}
-      walletConnectors={connectors}
-    >
+    <ThirdwebProvider activeChain={activeChainId} supportedWallets={[magicLinkConfig]}>
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
