4718ae29768ee32e7881c8557738ce0da4214313
John Cruz <johncruzag@gmail.com>
2023-06-23 11:16:19 -0400

    thirdweb upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,22 @@
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
-import { MagicConnector } from "@thirdweb-dev/react/evm/connectors/magic";
+//import { MagicConnector } from "@thirdweb-dev/react/evm/connectors/magic";
+import { magicLink } from "@thirdweb-dev/react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Polygon;
 
-const magicLinkConnector = new MagicConnector({
-  options: {
-    apiKey: process.env.NEXT_PUBLIC_MAGIC_LINK_API_KEY as string,
-    rpcUrls: {
-      [ChainId.Polygon]: "https://rpc-mainnet.matic.quiknode.pro",
-    },
-  },
-});
+// const magicLinkConnector = new MagicConnector({
+//   options: {
+//     apiKey: process.env.NEXT_PUBLIC_MAGIC_LINK_API_KEY as string,
+//     rpcUrls: {
+//       [ChainId.Polygon]: "https://rpc-mainnet.matic.quiknode.pro",
+//     },
+//   },
+// });
+
+const magicLinkConnector =  magicLink({ apiKey: process.env.NEXT_PUBLIC_MAGIC_LINK_API_KEY as string,  type: 'connect' })
 
 // Array of wallet connectors you want to use for your dApp.
 const connectors = [magicLinkConnector];
